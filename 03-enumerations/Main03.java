013b7ff042fcdc151997a1a6862460eb2c218aaf
Choopong Choosamer <choopong.choosamer@lazada.co.th>
2018-04-03 10:32:46 +0700

    Updated 03-enumerations

--- 03-enumerations/Main03.java
+++ 03-enumerations/Main03.java
@@ -14,10 +14,67 @@ public class Main03 {
         System.out.println(day1 == day3);
 
         System.out.println(day1);
+
+        System.out.println(DayWithColor.MONDAY.getColor());
+
+        System.out.println(PRODUCT.APPLE.IPHONE_8);
     }
 
     public enum Day {
         SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
         THURSDAY, FRIDAY, SATURDAY
+    }
+
+    //Enum with function
+    public enum DayWithColor {
+        SUNDAY {
+            public String getColor() {
+                return "Red";
+            }
+        },
+        MONDAY {
+            public String getColor() {
+                return "Yellow";
+            }
+        },
+        TUESDAY {
+            public String getColor() {
+                return "Pink";
+            }
+        },
+        WEDNESDAY {
+            public String getColor() {
+                return "Green";
+            }
+        },
+        THURSDAY {
+            public String getColor() {
+                return "Orange";
+            }
+        },
+        FRIDAY {
+            public String getColor() {
+                return "Blue";
+            }
+        },
+        SATURDAY {
+            public String getColor() {
+                return "Purple";
+            }
+        };
+        public abstract String getColor();
+    }
+
+    //Nested Enum
+    public enum PRODUCT {
+        SONY;
+        enum APPLE {
+            IPHONE_8,
+            IPHONE_X
+        }
+        enum SAMSUNG {
+            S7,
+            S8
+        }
     }
 }
